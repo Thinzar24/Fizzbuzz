5ff80d735fcda6853699e5ab5ea184c8f1158234
ZoeyHtut <zoeymhtut@yahoo.com>
2018-06-08 11:50:09 -0400

    Multiple of Seven version of FizzBuzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.*;
 
 
@@ -19,52 +20,37 @@ public class Main {
 
         startnum = 1 + r.nextInt(100);
         endnum = 1 + r.nextInt(100);
-        System.out.println(startnum);
-        System.out.println(endnum);
-        if(startnum == endnum) {
+
+
+        int digt = endnum - startnum; // variable for digt
+
+        if (startnum == endnum) { // if first and end is same print out shazam
+            System.out.println(startnum);
+            System.out.println(endnum);
             System.out.println("Shazam!");
+
+            // if start & last number are last tha 10 print won't take long
+        } else if (digt < 10) {
+            System.out.println("This won't take long!");
         }
 
         for (int i = startnum; i < endnum; i++) { // for loop to print 1 -100
 
             if (i % 3 == 0 && i % 5 == 0) { // 3 and 5 have 0 modulo print fizzbuzz
-                    System.out.println("FizzBuzz");
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) { // 3 had 0 modulo print fizz
                 System.out.println("Fizz");
             } else if (i % 5 == 0) { // 5 had 0 modulo print buzz
                 System.out.println("Buzz");
-            } else {
+            }else if (i % 7 == 0){
+                  System.out.println(i + " multiple of serven ");
+
+                } else {
                 System.out.println(i);
 
+
             }
-//            if (endnum < startnum) {
-//                System.out.println(startnum);
-//                System.out.println(endnum);
-//            } else if (startnum == endnum) {
-//                System.out.println("Shazam!!");
-//
-//
-//            }
         }
-
-   /* public static void Shazam() {
-
-     for (int i = 1; i <= 101; i++) {
-
-            if (endnum < startnum) {
-                System.out.println(startnum);
-                System.out.println(endnum);
-                System.out.println("First and Last  number should not be same ");
-            } else if (startnum == endnum) {
-                System.out.println(" Shazam !");
-            } else {
-                System.out.println(i);
-            }
-
-        }
-
-    }*/
-
     }
 }
 
